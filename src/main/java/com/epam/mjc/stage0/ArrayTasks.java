cd10934b7b1954e384f33e3baccf53e008a1836c
AliAmet <ametov1ali@gmail.com>
2023-11-12 10:12:05 +0600

    first commit

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -11,7 +11,8 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-
+        String seasons[] = {"winter", "spring", "summer", "autumn"};
+        return seasons;
     }
 
     /**
@@ -25,7 +26,11 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-
+        int arr[] = new int[length];
+        for (int i = 1; i <=arr.length; i++){
+            arr[i] = i;
+        }
+        return arr;
     }
 
     /**
@@ -37,7 +42,11 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        return sum;
     }
 
     /**
@@ -50,7 +59,14 @@ public class ArrayTasks {
      * arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        int index = 0;
+        for (int i = 1; i <= arr.length; i++){
+            if (arr[i] == number)
+                index = i;
+            else
+                index = -1;
+        }
+        return index;
     }
 
     /**
@@ -63,7 +79,11 @@ public class ArrayTasks {
      * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-
+        String[] arr2 = new String[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            arr2[i] = arr[arr.length - 1 - i];
+        }
+        return arr2;
     }
 
     /**
@@ -78,7 +98,12 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-
+        int arr2[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > 0)
+                arr[2] = arr[i];
+        }
+        return arr2;
     }
 
     /**
@@ -91,7 +116,7 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
-    public int[][] sortRaggedArray(int[][] arr) {
-
-    }
+//    public int[][] sortRaggedArray(int[][] arr) {
+//        return 0;
+//    }
 }
--- src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
+++ src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
@@ -4,6 +4,25 @@ import com.epam.mjc.stage0.utils.Fridge;
 
 public class FridgeAlgorithm {
 
+    Fridge fridge = new Fridge() {
+        @Override
+        public void close() {
+            System.out.println("Close th fridge");
+        }
+
+        @Override
+        public void open() {
+
+        }
+
+        @Override
+        public void getMilk() {
+            System.out.println("Get the milk");
+        }
+    };
+
+
+
     /**
      * This is a first task in this module. You need to get milk from a fridge =)
      * <p>
